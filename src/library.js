e2f10b0977c5a7e94f556be8a5afb5b20a2ac8d6
Dastan-Asl <63645943+Dastan-Asl@users.noreply.github.com>
2021-04-17 16:02:20 +0300

    queve list films

--- src/library.js
+++ src/library.js
@@ -1,1 +1,11 @@
+import queveListFilms from './js/queve-films';
+
 const key = 'aef9cffb51e8fe7e1c3e621e64df0279';
+const buttonRef = document.querySelector('.js-queve-button');
+
+const listMarkup = () => {
+  const arr = JSON.parse(localStorage.getItem('queve'));
+  arr.forEach(id => queveListFilms(id));
+};
+
+buttonRef.addEventListener('click', listMarkup);
