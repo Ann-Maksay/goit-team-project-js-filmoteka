e8bc5bcd4c003170b6e5e20286e2b9bd95b0f884
Dastan-Asl <63645943+Dastan-Asl@users.noreply.github.com>
2021-04-17 17:20:35 +0300

    add chek

--- src/library.js
+++ src/library.js
@@ -2,10 +2,16 @@ import queveListFilms from './js/queve-films';
 
 const key = 'aef9cffb51e8fe7e1c3e621e64df0279';
 const buttonRef = document.querySelector('.js-queve-button');
+const ulRef = document.querySelector('.film-list');
 
 const listMarkup = () => {
-  const arr = JSON.parse(localStorage.getItem('queve'));
-  arr.forEach(id => queveListFilms(id));
+  ulRef.innerHTML = '';
+  if (localStorage.getItem('queue')) {
+    const arr = JSON.parse(localStorage.getItem('queue'));
+    arr.forEach(id => queveListFilms(id));
+  } else {
+    ulRef.innerHTML = 'Ваша очередь пока пуста.';
+  }
 };
 
 buttonRef.addEventListener('click', listMarkup);
