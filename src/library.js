dfe9a61314f9b27ed3e07d5746f63573aa288bc4
Ann Maksay <annaorlova.dn@gmail.com>
2021-04-18 23:36:02 +0300

    Поправила контейнеры

--- src/library.js
+++ src/library.js
@@ -5,6 +5,12 @@ const buttonQueveRef = document.querySelector('.js-queve-button');
 const buttonWatchedRef = document.querySelector('.js-watched-button');
 const ulRef = document.querySelector('.film-list');
 
+const queueArry = ['299536', '383498', '500664', '466282', '455980', '489931', '421792'];
+const watchedArry = ['429300', '857', '470918', '353081', '271706', '333339', '347375'];
+
+localStorage.setItem('queue', JSON.stringify(queueArry));
+localStorage.setItem('watched', JSON.stringify(watchedArry));
+
 const listMarkup = () => {
 
   if (buttonWatchedRef.classList.contains('active-btn')) {
