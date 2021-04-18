46aae604285762eed37894be55af31e62d128dca
Ann Maksay <annaorlova.dn@gmail.com>
2021-04-18 20:11:12 +0300

    Синхронизация кнопок в библиотеке

--- src/library.js
+++ src/library.js
@@ -1,10 +1,19 @@
 import queveListFilms from './js/queve-films';
 
 const key = 'aef9cffb51e8fe7e1c3e621e64df0279';
-const buttonRef = document.querySelector('.js-queve-button');
+const buttonQueveRef = document.querySelector('.js-queve-button');
+const buttonWatchedRef = document.querySelector('.js-watched-button');
 const ulRef = document.querySelector('.film-list');
 
 const listMarkup = () => {
+
+  if (buttonWatchedRef.classList.contains('active-btn')) {
+    buttonWatchedRef.classList.remove('active-btn');
+  }
+  if (!buttonQueveRef.classList.contains('active-btn')) {
+    buttonQueveRef.classList.add('active-btn');
+  }
+
   ulRef.innerHTML = '';
   if (localStorage.getItem('queue')) {
     const arr = JSON.parse(localStorage.getItem('queue'));
@@ -14,5 +23,5 @@ const listMarkup = () => {
   }
 };
 
-buttonRef.addEventListener('click', listMarkup);
+buttonQueveRef.addEventListener('click', listMarkup);
 
